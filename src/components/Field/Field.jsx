a6f707d977b5c6916cd6e0d2ab333d8722d82348
Anton Krupin <krupin_anton@mail.ru>
2023-02-27 21:28:31 +0300

    add generate random numbers function

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -12,6 +12,24 @@ const Field = (props) => {
   for (let i = 0; i < size; i += 1) {
     field.push({ id: i });
   }
+  /*
+    const result = [];
+
+    const getRandomNumber = (max) => {
+      return Math.floor(Math.random() * max);
+    }
+
+    while(result.length < 40) {
+      const temp = getRandomNumber(256);
+      if (result.indexOf(temp) === -1) {
+        result.push(temp);
+      }
+    }
+
+    result.sort(function(a, b) {
+      return a - b;
+    });
+  */
 
   return (
     <div className="field">
