4dce61ae4ce980fa128063cc826c4c8247cac7fd
antonkrupin <krupin_anton@mail.ru>
2023-02-28 10:03:05 +0300

    npm install redux

--- src/components/Header/StartButton.jsx
+++ src/components/Header/StartButton.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import './StartButton.css';
 
-const StartButton = () => {
-  return (
-    <button
-      type="button"
-      aria-label="start-button"
-    />
-  );
-};
+const StartButton = () => (
+  <button
+    type="button"
+    aria-label="start-button"
+  />
+);
 
 export default StartButton;
