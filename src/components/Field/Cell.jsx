19b0dfbf9a0deddaf26e2fc6241a03e8d80b6937
antonkrupin <krupin_anton@mail.ru>
2023-02-28 13:34:50 +0300

    update click handlers

--- src/components/Field/Cell.jsx
+++ src/components/Field/Cell.jsx
@@ -1,35 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import cn from 'classnames';
 
-// import { getCellStatus, changeCellStatus } from '../../slices/cellReducer';
+import { changeCellStatus } from '../../slices/cellReducer';
 
 import './Cell.css';
 
 const Cell = (props) => {
-  const { id, clicked } = props;
-  // const dispatch = useDispatch();
+  const { id } = props;
+  const dispatch = useDispatch();
 
-  const [isClicked, setIsClicked] = useState(clicked);
+  const cells = useSelector((state) => state.cell.field);
+  const [cell] = cells.flat().filter((c) => c.id === id);
 
-  const [isRightClicked, setIsRightClicked] = useState(false);
-
-  const clickHandler = () => {
-    setIsClicked(!isClicked);
+  const clickHandler = (e) => {
+    if (cell.status === 'closed') {
+      dispatch(changeCellStatus({ id, type: e.type }));
+    }
   };
 
   const rightClickHandler = (e) => {
-    e.preventDefault();
-    setIsRightClicked(!isRightClicked);
+    e.preventDefault(e);
+    if (cell.status !== 'opened') {
+      dispatch(changeCellStatus({ id, type: e.type }));
+    }
   };
+
+  const className = cn('cell', {
+    opened: cell.status === 'opened',
+    flagged: cell.status === 'flagged',
+    question: cell.status === 'question',
+  });
 
   return (
     <button
       id={id}
       type="button"
       aria-label="cell"
-      className={`cell ${isClicked && 'opened'} ${isRightClicked && 'flagged'}`}
+      className={className}
       onClick={clickHandler}
       onContextMenu={rightClickHandler}
     />
