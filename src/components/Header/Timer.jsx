ef0f09940e6738731a2d73ddda705658635ad3b1
antonkrupin <krupin_anton@mail.ru>
2023-02-28 15:57:03 +0300

    change Timer component

---
+++ src/components/Header/Timer.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { useStopwatch } from 'react-timer-hook';
+
+import './Timer.css';
+
+const Timer = () => {
+  const {
+    seconds,
+    minutes,
+  } = useStopwatch({ autoStart: true });
+
+  const path1 = `./img/timer/number_${seconds > 9 ? `${seconds % 10}` : `${seconds}`}.png`;
+  const path2 = `./img/timer/number_${seconds < 100 && seconds > 9 ? `${seconds % 100}`[0] : '0'}.png`;
+  const path3 = `./img/timer/number_${seconds < 1000 && seconds > 99 ? `${seconds % 1000}`[0] : '0'}.png`;
+
+  return (
+    <div className="timer">
+      <div style={{ fontSize: '20px' }}>
+        <span className="minutes">{minutes}</span>
+        <span className="seconds">
+          <img src={path3} alt="timer" />
+          <img src={path2} alt="timer" />
+          <img src={path1} alt="timer" />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
