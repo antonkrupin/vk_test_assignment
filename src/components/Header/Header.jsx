b00acec0377e2bee00d5aed0349856e639c6b249
antonkrupin <krupin_anton@mail.ru>
2023-02-28 15:57:48 +0300

    change Header component;

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import StartButton from './StartButton';
+import Timer from './Timer';
 
 import './Header.css';
 
 const Header = () => {
-  const test = '1';
+  const gameStatus = useSelector((state) => state.cell.gameStatus);
   return (
     <div className="header">
-      <h1><StartButton /></h1>
-      <h2>{test}</h2>
+      <Timer />
+      <StartButton />
+      <Timer start={gameStatus} />
     </div>
   );
 };
