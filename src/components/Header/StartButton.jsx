b3ace8d95aa7a6abda1927386e99e4e3c396da9c
Anton Krupin <krupin_anton@mail.ru>
2023-02-28 20:29:32 +0300

    change StartButton component

--- src/components/Header/StartButton.jsx
+++ src/components/Header/StartButton.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import cn from 'classnames';
 
 import './StartButton.css';
 
-const StartButton = () => (
-  <button
-    type="button"
-    aria-label="start-button"
-  />
-);
+const StartButton = () => {
+  const gameStatus = useSelector((state) => state.cell.gameStatus);
+
+  const className = cn('start', {
+    lose: gameStatus === 'lose',
+  });
+
+  return (
+    <button
+      className={className}
+      type="button"
+      aria-label="start-button"
+    />
+  );
+};
 
 export default StartButton;
