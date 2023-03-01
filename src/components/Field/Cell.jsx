3b493f27a568a88020c1c190ced512d91a3c1548
antonkrupin <krupin_anton@mail.ru>
2023-03-01 13:13:20 +0300

    add mouseDownHandler, mouseLeaveHandler

--- src/components/Field/Cell.jsx
+++ src/components/Field/Cell.jsx
@@ -5,12 +5,12 @@ import { useSelector, useDispatch } from 'react-redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import cn from 'classnames';
 
-import { startGame, changeCellStatus } from '../../slices/cellReducer';
+import { changeGameStatus, changeCellStatus } from '../../slices/cellReducer';
 
 import './Cell.css';
 
 const Cell = (props) => {
-  const { id } = props;
+  const { id, lineId } = props;
   const dispatch = useDispatch();
 
   const cells = useSelector((state) => state.cell.field);
@@ -18,10 +18,12 @@ const Cell = (props) => {
   const [cell] = cells.flat().filter((c) => c.id === id);
 
   const clickHandler = (e) => {
+    console.log(lineId);
     if (!gameStatus) {
-      dispatch(startGame());
+      dispatch(changeGameStatus('start'));
     }
     if (cell.status === 'closed') {
+      dispatch(changeGameStatus('start'));
       dispatch(changeCellStatus({ id, type: e.type }));
     }
   };
@@ -29,11 +31,22 @@ const Cell = (props) => {
   const rightClickHandler = (e) => {
     e.preventDefault(e);
     if (!gameStatus) {
-      dispatch(startGame());
+      dispatch(changeGameStatus('start'));
     }
     if (cell.status !== 'opened') {
+      dispatch(changeGameStatus('start'));
       dispatch(changeCellStatus({ id, type: e.type }));
     }
+  };
+
+  const mouseDownHandler = () => {
+    if (cell.status === 'closed') {
+      dispatch(changeGameStatus('waiting'));
+    }
+  };
+
+  const mouseLeaveHandler = () => {
+    dispatch(changeGameStatus('start'));
   };
 
   const className = cn('cell', {
@@ -52,7 +65,9 @@ const Cell = (props) => {
       aria-label="cell"
       className={className}
       onClick={clickHandler}
+      onMouseDown={mouseDownHandler}
       onContextMenu={rightClickHandler}
+      onMouseLeave={mouseLeaveHandler}
     />
   );
 };
