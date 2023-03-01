cdfd3d0e23505f9b30b78b433019b24bcb30e95f
antonkrupin <krupin_anton@mail.ru>
2023-03-01 13:16:02 +0300

    change Field component

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -6,15 +6,13 @@ import CellLine from './CellLine';
 
 import './Field.css';
 
-const Field = (props) => {
-  const { size } = props;
-
+const Field = () => {
   const field = useSelector((state) => state.cell.field);
 
   return (
     <div className="field">
       {field.map((line) => (
-        <CellLine key={line[0]} id={line.id} size={size} line={line.slice(1)} />
+        <CellLine key={line[0]} id={line[0]} line={line.slice(1)} />
       ))}
     </div>
   );
